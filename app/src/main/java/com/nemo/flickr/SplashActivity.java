a8222479616cf291878868895997b7dbe7dd535f
java2019 <java_2019@mail.ru>
2017-08-04 00:37:17 +0300

    Steps

--- app/src/main/java/com/nemo/flickr/SplashActivity.java
+++ app/src/main/java/com/nemo/flickr/SplashActivity.java
@@ -1,17 +1,40 @@
 package com.nemo.flickr;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import org.apache.http.NameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+import com.hintdesk.core.utils.JSONHttpClient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SplashActivity extends AppCompatActivity {
+
+    public static final String flickr_photosets_getPhotos = "https://www.flickr.com/services/rest/" +
+            "?method=flickr.photosets.getPhotos&format=json&api_key=73fc40a69463a80783a192152e174078&photoset_id=72157623480041879";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         try {
-            Thread.sleep(2000);
+            Thread.sleep(1000);
         } catch (Exception e) {
 
         }
@@ -19,5 +42,66 @@ public class SplashActivity extends AppCompatActivity {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    class LoadImagesFromFlickrTask extends AsyncTask<String, Integer, List> {
+        private ProgressDialog progressDialog;
+        private Integer totalCount, currentIndex;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            //progressDialog.setMessage(String.format("Loading images from Flickr %s/%s. Please wait...", values[0], values[1]));
+        }
+
+        @Override
+        protected List doInBackground(String... params) {
+
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+            String resultJson = "";
+
+            try{
+                URL url = new URL(flickr_photosets_getPhotos);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line);
+                }
+
+                resultJson = buffer.toString();
+
+            }catch (Exception e){
+
+            }
+
+            JSONObject Photos = null;
+            try {
+                Photos = new JSONObject(resultJson);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+           return new ArrayList();
+        }
+
+
+        protected void onPostExecute(ArrayList s) {
+            super.onPostExecute(s);
+        }
     }
 }
