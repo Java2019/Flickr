95134a5e70adced729af6d95d76e6472a1b77da5
java2019 <java_2019@mail.ru>
2017-08-04 15:02:18 +0300

    Steps

--- app/src/main/java/com/nemo/flickr/FavoritesActivity.java
+++ app/src/main/java/com/nemo/flickr/FavoritesActivity.java
@@ -1,7 +1,10 @@
 package com.nemo.flickr;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.io.File;
 
 public class FavoritesActivity extends AppCompatActivity {
 
@@ -10,6 +13,9 @@ public class FavoritesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorites);
 
+        File folder = new File(Environment.getExternalStorageDirectory() +
+                File.separator + "FlickrImages");
+        File[] filelist = folder.listFiles();
 
     }
 }
