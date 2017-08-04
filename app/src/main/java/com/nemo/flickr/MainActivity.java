0d95ab317acc54468d56bd88aaa8956538795e21
java2019 <java_2019@mail.ru>
2017-08-04 10:22:47 +0300

    Steps

--- app/src/main/java/com/nemo/flickr/MainActivity.java
+++ app/src/main/java/com/nemo/flickr/MainActivity.java
@@ -1,28 +1,37 @@
 package com.nemo.flickr;
 
 import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.ImageView;
 
 import org.apache.http.NameValuePair;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+
 import com.hintdesk.core.utils.JSONHttpClient;
 import com.nemo.flickr.flickrutil.JSONObject.PhotosJSON;
 import com.nemo.flickr.flickrutil.Size;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String flickr_photosets_getPhotos = "https://www.flickr.com/services/rest/?method=flickr.photosets.getPhotos&format=json&api_key=73fc40a69463a80783a192152e174078&photoset_id=72157623480041879";
+    public static final String flickr_photosets_getPhotos = "https://www.flickr.com/services/rest/?method=flickr.photosets.getPhotos&format=json&nojsoncallback=1&api_key=73fc40a69463a80783a192152e174078&photoset_id=72157623480041879";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    class LoadImagesFromFlickrTask extends AsyncTask<String, Integer, List> {
+    class LoadImagesFromFlickrTask extends AsyncTask<Void, Void, Bitmap> {
         private ProgressDialog progressDialog;
         private Integer totalCount, currentIndex;
 
@@ -43,17 +52,19 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
-        protected void onProgressUpdate(Integer... values) {
+        protected void onProgressUpdate(Void... values) {
             super.onProgressUpdate(values);
             //progressDialog.setMessage(String.format("Loading images from Flickr %s/%s. Please wait...", values[0], values[1]));
         }
 
         @Override
-        protected List doInBackground(String... params) {
+        protected Bitmap doInBackground(Void... params) {
 
             HttpURLConnection urlConnection = null;
             BufferedReader reader = null;
             String resultJson = "";
+            String linkPhoto = "";
+            Bitmap bitmap = null;
 
             try{
                 URL url = new URL(flickr_photosets_getPhotos);
@@ -76,17 +87,55 @@ public class MainActivity extends AppCompatActivity {
             }catch (Exception e){
 
             }
+            try{
+            JSONObject json = new JSONObject(resultJson);
+            JSONObject photos = json.getJSONObject("photoset");
+            JSONArray photo = photos.getJSONArray("photo");
+            if (photo.length() > 0) {
 
-            JSONHttpClient jsonHttpClient = new JSONHttpClient();
-            PhotosJSON photosJSON  = jsonHttpClient.Get(flickr_photosets_getPhotos, new ArrayList<NameValuePair>(), PhotosJSON.class);
-            List<Size>  sizes = photosJSON.getSizes().getSize();
-            return new ArrayList();
+                JSONObject first = photo.getJSONObject(0);
+
+                String FARMID = first.getString("farm");
+                String SERVERID = first.getString("server");
+                String SECRET = first.getString("secret");
+                String ID = first.getString("id");
+
+                StringBuilder sb = new StringBuilder();
+
+                sb.append("http://farm");
+                sb.append(FARMID);
+                sb.append(".static.flickr.com/");
+                sb.append(SERVERID);
+                sb.append("/");
+                sb.append(ID);
+                sb.append("_");
+                sb.append(SECRET);
+                sb.append("_m");
+                sb.append(".jpg");
+
+                linkPhoto = sb.toString();
+
+                bitmap = BitmapFactory.decodeStream((InputStream)new URL(linkPhoto).getContent());
+
+            } } catch (JSONException e) {
+                e.printStackTrace();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return bitmap;
         }
 
-
-        protected void onPostExecute(ArrayList s) {
-            super.onPostExecute(s);
+        protected void onPostExecute(Bitmap bitmap) {
+            ImageView i = (ImageView)findViewById(R.id.image);
+            i.setImageBitmap(bitmap);
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
 }
